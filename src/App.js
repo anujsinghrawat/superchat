fd06d6208cb85a6a9bb016d7af655d0f7527734d
anujsinghrawat <2003013019@ipec.org.in>
2022-08-13 22:06:23 +0530

    superchat
    Signed-off-by: anujsinghrawat <2003013019@ipec.org.in>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,138 @@
-import logo from './logo.svg';
+import React, { useRef, useState, useEffect} from 'react';
 import './App.css';
 
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection,  query, orderBy, limit, serverTimestamp, addDoc } from 'firebase/firestore';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import 'firebase/compat/analytics';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCvcnFcg7W2O-JlVOJEbuVY0fTBFz7sSIw",
+  authDomain: "superchat-aa158.firebaseapp.com",
+  projectId: "superchat-aa158",
+  storageBucket: "superchat-aa158.appspot.com",
+  messagingSenderId: "341060777164",
+  appId: "1:341060777164:web:b9f83fa3d5d208186abe69",
+  measurementId: "G-F6QCCZVCH7"
+};
+
+initializeApp(firebaseConfig);
+// init firestore services
+const db = getFirestore();
+const auth = getAuth();
+//const analytics = firebase.analytics();
+
+
+
 function App() {
+
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>⚛️🔥💬</h1>
+        <SignOut />
       </header>
+
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
+
     </div>
   );
 }
+
+function SignIn() {
+
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+  }
+
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+      <p>Do not violate the community guidelines or you will be banned for life!</p>
+    </>
+  )
+
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+
+function ChatRoom() {
+  const dummy = useRef();
+  const messageInput = useRef();
+  const messagesRef = collection(db, 'messages');
+  const q = query(messagesRef, orderBy('createdAt', 'desc'), limit(25));
+
+  const [messages] = useCollectionData(q);
+
+  const [formValue, setFormValue] = useState('');
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await addDoc(messagesRef, {
+      uid,
+      photoURL,
+      text: formValue,
+      createdAt: serverTimestamp()
+    })
+
+    messageInput.current.focus();
+    setFormValue('');
+    //dummy is updated when the component is re-rendered, instead of after a message is sent.
+  }
+
+  //Everytime messages is modified, will use the dummy reference to scroll to the bottom.
+  useEffect(() => {
+    dummy.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [messages]);
+
+  return (<>
+    <main>
+
+      {messages && messages.slice(0).reverse().map((msg, idx) => <ChatMessage key={idx} message={msg} />)}
+    
+      <div ref={dummy}/>
+
+    </main>
+
+    <form onSubmit={sendMessage}>
+
+      <input ref={messageInput} value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+
+      <button type="submit" disabled={!formValue}>🕊️</button>
+
+    </form>
+  </>)
+}
+
+
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return (<>
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} referrerPolicy="no-referrer" />
+      <p>{text}</p>
+    </div>
+  </>)
+}
+
 
 export default App;
